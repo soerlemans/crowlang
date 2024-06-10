2ccf105a980502afefe48c5c40e5275708f8b0a3
hackerman <hackerman@ConnectON>
2024-06-10 19:00:49 +0200

    I need to fix the release build, by having Cereal recognize the JUST_INSTALL_CEREAL option.

--- cmake/dependencies/cereal.cmake
+++ cmake/dependencies/cereal.cmake
@@ -1,14 +1,14 @@
 # Cereal (C++ serialization library):
 message(STATUS "[+] Fetching cereal library.")
 
+set(JUST_INSTALL_CEREAL OFF CACHE INTERNAL "Only build Cereal.")
+
 FetchContent_Declare(
   cereal
   GIT_REPOSITORY https://github.com/USCiLab/cereal.git
   GIT_TAG v1.3.2
   GIT_PROGRESS TRUE
-	CMAKE_ARGS
-	-DJUST_INSTALL_CEREAL=ON
-	-DSKIP_PORTABILITY_TEST=ON
+	CMAKE_ARGS -DJUST_INSTALL_CEREAL=ON # -DSKIP_PORTABILITY_TEST=ON
 	SYSTEM
 )
 
