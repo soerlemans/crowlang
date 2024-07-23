8ae8e5644297af40b14921044e5090b2991408f8
hackerman <hackerman@ConnectON>
2024-07-23 23:55:05 +0200

    Doing some basic work on the standard library.

--- src/stdlib/io.hpp
+++ src/stdlib/io.hpp
@@ -2,8 +2,11 @@
 #define CROW_STDLIB_IO_HPP
 
 namespace stdlib {
-inline auto println() -> void
-{}
+template<typename... Args>
+inline auto println(std::format_string<Args...> t_fmt, Args&&... t_args) -> void
+{
+  std::println(fmt, std::forward<Args>(t_args)...);
+}
 } // namespace stdlib
 
 #endif // CROW_STDLIB_IO_HPP
