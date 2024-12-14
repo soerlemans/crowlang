a8cdf279fda6dc1d85e83533396daf3b128221cf
soerlemans <sebasoerlemans+git@gmail.com>
2024-12-14 11:47:04 +0100

    Fixing stdlibcrow being untracked.

---
+++ src/stdlibcrow/regex.hpp
@@ -0,0 +1,12 @@
+#ifndef REGEX_HPP
+#define REGEX_HPP
+
+// STL Includes:
+#include <format>
+#include <print>
+
+namespace stdlibcrow {
+// TODO: Regex.
+} // namespace stdlibcrow
+
+#endif // REGEX_HPP
