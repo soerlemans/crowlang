c6af6d38220eca911458f0301c61b9aef9dabf39
soerlemans <sebastiaan.oerlemans@zimperium.com>
2026-01-01 12:40:44 +0300

    ci-cd: Formatted.

--- src/crow/ast/visitor/ast_archive.hpp
+++ src/crow/ast/visitor/ast_archive.hpp
@@ -75,8 +75,7 @@ class AstArchive : public NodeVisitor {
       t_archive(std::forward<Args>(t_args)...);
     }};
 
-    const auto fn_monostate{[]([[maybe_unused]]
-                               std::monostate t_state) {
+    const auto fn_monostate{[]([[maybe_unused]] std::monostate t_state) {
       lib::stdexcept::throw_exception("Attempted to archive std::monostate.");
     }};
 
