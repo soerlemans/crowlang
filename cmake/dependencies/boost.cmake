d9a848dfe60c50e6820fcad60ca30f01d4efca42
soerlemans <sebastiaan.oerlemans@zimperium.com>
2026-01-01 14:22:54 +0300

    ci-cd: Fixing deps.

--- cmake/dependencies/boost.cmake
+++ cmake/dependencies/boost.cmake
@@ -1,18 +1,10 @@
 # Boost (C++ utility libraries):
 message(STATUS "[+] Finding Boost library.")
 
-#FetchContent_Declare(
-#    Boost
-#    GIT_REPOSITORY https://github.com/boostorg/boost.git
-#    GIT_TAG boost-1.89.0
-#)
-#
-#FetchContent_MakeAvailable(Boost)
-
 find_package(
   Boost
   1.89
-  COMPONENTS system program_options filesystem iostreams thread
+  COMPONENTS program_options filesystem iostreams thread
   REQUIRED
 )
 
